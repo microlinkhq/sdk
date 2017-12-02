d0d82424a7f1185eb70c2417011b380d8be69e40
Brad Adams <hi@breadadams.com>
2017-12-02 07:10:20 +0100

    Add Card to Storybook stories 📚

--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import Button from './Button';
-import Welcome from './Welcome';
+// @flow
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+// import { action } from '@storybook/addon-actions'
+// import { linkTo } from '@storybook/addon-links'
+import { MicrolinkCard } from '../src/'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Card', module).add('small', () => (
+  <MicrolinkCard url="http://bbc.com" />
+))
