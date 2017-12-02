dd5af7bed925d5fd7f8823ece76dca54beb28487
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-02 13:24:27 +0100

    Customize border radius

--- src/components/Card/CardWrap.js
+++ src/components/Card/CardWrap.js
@@ -1,13 +1,21 @@
 // @flow
 import styled from 'styled-components'
 
+const DEFAULT = {
+  rounded: '.42857em'
+}
+
+const getValue = (props, name) => typeof props[name] === 'string'
+  ? props[name]
+  : DEFAULT[name]
+
 const CardWrap = styled.a`
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   background: #fff;
   color: #181919;
   max-width: 500px;
-  border-radius: 4px;
-  border: 1px solid #efefef;
+  border-radius: ${props => props.rounded ? getValue(props, 'rounded') : '0px'};
+  border: 1px solid #E1E8ED;
   overflow: hidden;
   display: flex;
   height: 132px;
