e9c3513e5957312aac4dc95329e822497485c2bc
Brad Adams <hi@breadadams.com>
2017-12-03 17:10:40 +0100

    Refactor CardImage

--- src/components/Card/CardImage.js
+++ src/components/Card/CardImage.js
@@ -1,12 +1,15 @@
 // @flow
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { CardImageLarge } from './CardLarge'
 
 export default styled.div`
   display: block;
   flex: 0 0 125px;
   background: no-repeat center center / cover;
-  background-image: ${props => props.image ? `url(${props.image})` : ``};
+
+  ${({ image }) => image && css`
+    background-image: url(${image});
+  `};
 
   &::before {
     content: '';
