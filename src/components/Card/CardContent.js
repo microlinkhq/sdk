ff89fd85ce45f25dc373b68c96995c1b041dc739
Brad Adams <hi@breadadams.com>
2017-12-03 05:25:45 +0100

    Restructuring of CardEmptyState

--- src/components/Card/CardContent.js
+++ src/components/Card/CardContent.js
@@ -6,11 +6,10 @@ import extractDomain from 'extract-domain'
 type ContentProps = {
   title?: string,
   description?: string,
-  url?: string,
-  empty?: boolean
+  url?: string
 }
 
-const ContentWrap = styled.div`
+export const ContentWrap = styled.div`
   flex: 1;
   padding: 15px;
   min-width: 0;
@@ -46,46 +45,8 @@ const Url = styled.span`
   display: inline-block;
 `
 
-const EmptyContent = styled.div`
-  opacity: 0.5;
-`
-
-const EmptyTitle = styled.span`
-  height: 16px;
-  width: 80%;
-  display: block;
-  background: #e1e8ed;
-  margin-bottom: 8px;
-`
-const EmptyDescription = styled.span`
-  height: 54px;
-  width: 100%;
-  display: block;
-  background: #e1e8ed;
-  margin-bottom: 12px;
-`
-const EmptyLink = styled.span`
-  height: 12px;
-  width: 60%;
-  display: block;
-  background: #e1e8ed;
-`
-
-const CardContent = ({
-  title,
-  description,
-  url,
-  logo,
-  empty = false
-}: ContentProps) => (
+const CardContent = ({ title, description, url }: ContentProps) => (
   <ContentWrap>
-    {empty && (
-      <EmptyContent>
-        <EmptyTitle />
-        <EmptyDescription />
-        <EmptyLink />
-      </EmptyContent>
-    )}
     {title && <Title title={title}>{title}</Title>}
     {description && <Text>{description}</Text>}
     {url && <Url>{extractDomain(url)}</Url>}
--- src/components/Card/CardEmptyState.js
+++ src/components/Card/CardEmptyState.js
@@ -1,11 +1,43 @@
 // @flow
 import React from 'react'
+import styled from 'styled-components'
 
 import CardImage from './CardImage'
-import CardContent from './CardContent'
+import { ContentWrap } from './CardContent'
+
+const EmptyTitle = styled.span`
+  height: 16px;
+  width: 80%;
+  display: block;
+  background: #e1e8ed;
+  margin-bottom: 8px;
+  opacity: 0.8;
+`
+const EmptyDescription = styled.span`
+  height: 54px;
+  width: 100%;
+  display: block;
+  background: #e1e8ed;
+  margin-bottom: 12px;
+  opacity: 0.8;
+`
+const EmptyLink = styled.span`
+  height: 12px;
+  width: 60%;
+  display: block;
+  background: #e1e8ed;
+  opacity: 0.8;
+`
 
 const CardEmptyState = () => {
-  return [<CardImage />, <CardContent empty />]
+  return [
+    <CardImage />,
+    <ContentWrap>
+      <EmptyTitle />
+      <EmptyDescription />
+      <EmptyLink />
+    </ContentWrap>
+  ]
 }
 
 export default CardEmptyState
