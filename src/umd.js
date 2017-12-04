4440b58fc2f2b344014fe3565f0c663fb17aa91b
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-04 14:05:10 +0100

    Customize mount node

--- src/umd.js
+++ src/umd.js
@@ -1,19 +1,16 @@
 'use strict'
 
 const qa = require('dom101/query-selector-all')
-const each = require('dom101/each')
-
-const React = require('react')
 const ReactDOM = require('react-dom')
-const MicrolinkCard = require('./index.js').default
+const each = require('dom101/each')
+const React = require('react')
 
-module.exports = (selector, opts) => (
+const {default: MicrolinkCard} = require('./index.js')
+
+module.exports = (selector, {is = 'div', ...opts} = {}) => (
   each(qa(selector), el => {
     const url = el.getAttribute('href')
-
-    ReactDOM.render(
-      React.createElement(MicrolinkCard, { url }),
-      el
-    )
+    const card = React.createElement(MicrolinkCard, { url, is, ...opts })
+    ReactDOM.render(card, el)
   })
 )
