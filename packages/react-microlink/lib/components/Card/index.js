fab03e54accff38f2fd157e85d8adf6906d8a397
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-11 17:35:05 +0100

    Remove build from repository

--- packages/react-microlink/lib/components/Card/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CardContent } from './CardContent';
-export { default as CardEmptyState } from './CardEmptyState';
-export { default as CardImage } from './CardImage';
-export { default as CardWrap } from './CardWrap';
