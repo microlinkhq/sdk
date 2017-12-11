8eb2fc89c78bbe28c2592cbfc165087c30fa2f7a
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-11 23:17:08 +0100

    Update build

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 window.$docsify = {
   name: 'microlink.js',
   repo: 'microlinkhq/microlinkjs',
-  homepage: 'https://cdn.rawgit.com/microlinkhq/microlinkjs/master/README.md',
+  homepage: 'https://rawgit.com/microlinkhq/microlinkjs/master/README.md',
   subMaxLevel: 2,
   executeScript: true,
   auto2top: true,
