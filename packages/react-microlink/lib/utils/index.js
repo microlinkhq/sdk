c4c5f3aba9064c54d493c532a05a0a7c5387e6da
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-12-16 12:48:44 +0100

    Delete dist from repository

--- packages/react-microlink/lib/utils/index.js
+++
@@ -1,5 +0,0 @@
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-export var getUrlPath = function getUrlPath(data) {
-  return (typeof data === 'undefined' ? 'undefined' : _typeof(data)) === 'object' ? data.url : data;
-};
