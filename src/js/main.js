58d04e6819e4af5a66240f3b2d72c80f46476cd5
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-01-21 12:10:27 +0100

    Update doc

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-window.$docsify = {
-  name: 'microlink.js',
-  repo: 'microlinkhq/microlinkjs',
-  executeScript: true,
-  maxLevel: 3,
-  auto2top: true,
-  coverpage: true,
-  ga: 'UA-108549225-3'
-}
