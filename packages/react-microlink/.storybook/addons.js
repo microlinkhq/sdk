486a0d6036fdb5a062d77c13fd43d78f5c240dac
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-20 18:01:53 +0100

    Remove unused addons

--- packages/react-microlink/.storybook/addons.js
+++ packages/react-microlink/.storybook/addons.js
@@ -1,6 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
-import '@storybook/addon-actions/register'
-import '@storybook/addon-links/register'
 import '@storybook/addon-options/register'
 import '@storybook/addon-viewport/register'
