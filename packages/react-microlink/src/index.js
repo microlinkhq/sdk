b1b83783fd3c66e1d6a75897a1a9ba845880ab38
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-24 18:23:39 +0100

    Remove unnecessary React reference

--- packages/react-microlink/src/index.js
+++ packages/react-microlink/src/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment, Component} from 'react'
+import {Fragment, Component} from 'react'
 import PropTypes from 'prop-types'
 
 import {CardWrap, CardMedia, CardContent, CardEmptyState} from './components/Card'
