ce319653ae04c866baf8f2bf680fdfe90fe74df9
Brad Adams <hi@breadadams.com>
2018-02-26 19:23:13 +0100

    Refactor CardMedia & CardMedia/image

--- packages/react-microlink/src/components/Card/CardMedia/image.js
+++ packages/react-microlink/src/components/Card/CardMedia/image.js
@@ -1,6 +1,8 @@
 import MediaWrap from './wrap'
 
-export default MediaWrap.extend`
+export default MediaWrap.extend.attrs({
+  className: 'microlink_card__media_image'
+})`
   background: no-repeat center center / cover;
-  ${({image}) => image && `background-image: url(${image});`}
+  background-image: ${({image}) => `url('${image}')` || ''};
 `
--- packages/react-microlink/src/components/Card/CardMedia/index.js
+++ packages/react-microlink/src/components/Card/CardMedia/index.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react'
+import {createElement, Component} from 'react'
 
 import {getUrlPath} from '../../../utils'
 
@@ -12,26 +12,34 @@ export default class CardMedia extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loadingImage: this.props.image ? true : false
+      loadingImage: props.image ? true : false
     }
   }
 
-  render() {
+  imageLoaded() {
+    this.setState({loadingImage: false})
+  }
+
+  renderMedia() {
     const {loadingImage} = this.state
-    return (
-      <Fragment>
-        {!isVideo(this.props) ? (
-          <Image className="microlink_card__media_image" loading={loadingImage} {...this.props} />
-        ) : (
-          <Video {...this.props} loading={loadingImage} />
-        )}
-        {this.props.image && (
-          <ImageLoadCatcher
-            src={this.props.image}
-            onLoad={() => this.setState({loadingImage: false})}
-          />
-        )}
-      </Fragment>
-    )
+    const el = !isVideo(this.props) ? Image : Video
+    return createElement(el, {
+      ...this.props,
+      key: 'media',
+      loading: loadingImage
+    })
+  }
+
+  renderLoadCatcher() {
+    const {image} = this.props
+    return image && createElement(ImageLoadCatcher, {
+      key: 'imageLoader',
+      src: image,
+      onLoad: () => this.imageLoaded()
+    })
+  }
+
+  render() {
+    return [this.renderMedia(), this.renderLoadCatcher()]
   }
 }
