1c1138018679efce14c80d1d0edce05079c8b862
Brad Adams <hi@breadadams.com>
2018-02-28 21:10:00 +0100

    More linter fixes

--- packages/react-microlink/src/components/Card/CardMedia/Video/controls.js
+++ packages/react-microlink/src/components/Card/CardMedia/Video/controls.js
@@ -3,33 +3,33 @@ import styled from 'styled-components'
 export const PlayButton = styled.div`
   position: absolute;
   background: #fff;
-  transform: rotate(30deg) skewX(-30deg) scale(1,.866);
+  transform: rotate(30deg) skewX(-30deg) scale(1, 0.866);
   top: calc(50% - 11px);
   left: calc(50% - 11px);
   z-index: 2;
-  opacity: ${({visible}) => visible ? 1 : 0};
-  transition: opacity .25s ease-in-out;
+  opacity: ${({visible}) => (visible ? 1 : 0)};
+  transition: opacity 0.25s ease-in-out;
 
   &:before,
   &:after {
-  	content: '';
-  	position: absolute;
-  	background: inherit;
+    content: '';
+    position: absolute;
+    background: inherit;
   }
 
   &,
   &:before,
   &:after {
-  	width:  22px;
-  	height: 22px;
-  	border-top-right-radius: 30%;
+    width: 22px;
+    height: 22px;
+    border-top-right-radius: 30%;
   }
 
   &:before {
-  	transform: rotate(-135deg) skewX(-45deg) scale(1.414,.707) translate(0,-50%);
+    transform: rotate(-135deg) skewX(-45deg) scale(1.414, 0.707) translate(0, -50%);
   }
   &:after {
-  	transform: rotate(135deg) skewY(-45deg) scale(.707,1.414) translate(50%);
+    transform: rotate(135deg) skewY(-45deg) scale(0.707, 1.414) translate(50%);
   }
 `
 
@@ -45,10 +45,10 @@ export const ProgressBar = styled.div.attrs({
   background: #e1e8ed;
   height: 2px;
   border-top-right-radius: 2px;
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 
   .microlink_card:not(:hover) & {
     opacity: 0 !important;
-    transition-delay: .5s;
+    transition-delay: 0.5s;
   }
 `
