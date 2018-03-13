a458d136c88d13403da18750a18a47e0258458ae
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-03-13 17:38:52 +0100

    Bundle all dependencies

--- packages/microlinkjs/config/webpack.config.prod.js
+++ packages/microlinkjs/config/webpack.config.prod.js
@@ -56,13 +56,6 @@ module.exports = {
         .relative(paths.appSrc, info.absoluteResourcePath)
         .replace(/\\/g, '/')
   },
-  externals: {
-    'styled-components': {
-      commonjs: 'styled-components',
-      commonjs2: 'styled-components',
-      amd: 'styled-components',
-    },
-  },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
     // We placed these paths second because we want `node_modules` to "win"
