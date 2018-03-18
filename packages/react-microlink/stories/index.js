29be523cbbcbba83acb5d7a7b182098900c501ee
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-03-18 19:28:59 +0100

    Simplify storybook

--- packages/react-microlink/stories/index.js
+++ packages/react-microlink/stories/index.js
@@ -1,302 +1,65 @@
 import 'unfetch/polyfill'
 
 import React from 'react'
-import { storiesOf } from '@storybook/react'
+import {storiesOf} from '@storybook/react'
 
-import MicrolinkCard from '../src'
-import { urls, urlsVideo, getRandomSize } from './data'
+import Microlink from '../src'
+import {urls, urlsVideo, getRandomSize} from './data'
 
-storiesOf('Normal', module)
-  .addWithJSX('default', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard key={url} url={url} style={{ marginBottom: '20px' }} />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with round prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          round
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom round prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          round={getRandomSize([6, 10, 20, 30, 50, 9999])}
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom width', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{
-            marginBottom: '20px',
-            width: getRandomSize([300, 400, 500, 600, 700, 800])
-          }}
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom height', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{
-            marginBottom: '20px',
-            height: getRandomSize([75, 125, 150, 175, 200, 250])
-          }}
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with contrast prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          contrast
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom style', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{
-            marginBottom: '20px',
-            fontFamily: 'Nitti, "Microsoft YaHei", 微软雅黑, monospace',
-            boxShadow: '0 1px 4px 0 hsla(0, 0%, 0%, 0.2)'
-          }}
-          round
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with image prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          image={'logo'}
-          round
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with video media', () => (
-    <div>
-      {urlsVideo.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          round
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom data', () => (
-    <MicrolinkCard
-      url='https://microlink.io'
-      data={{
-        title: 'My Custom Title',
-        image: 'https://microlink.io/logo-trim.png',
-        description: 'My Custom Description',
-        url: 'https://microlink.io'
-      }}
-    />
-  ))
-  .addWithJSX('with reverse prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          reverse
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with empty state', () => (
-    <MicrolinkCard url='somesitethatwontresolve.com' />
-  ))
+const createMicrolink = props => (
+  <div
+    key={props.url}
+    style={{ display: 'flex', flexDirection: 'column', paddingBottom: '64px' }}>
+    <a href={props.url} style={{color: '#0366d6', marginBottom: '2rem'}}>{props.url}</a>
+    <Microlink size='normal' style={{marginBottom: '2rem'}} {...props} />
+    <Microlink size='large' {...props} />
+  </div>
+)
 
-storiesOf('Large', module)
-  .addWithJSX('default', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          size='large'
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with round prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          size='large'
-          round
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom round prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          size='large'
-          round={getRandomSize([6, 10, 20, 30])}
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom width', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{
-            marginBottom: '20px',
-            width: getRandomSize([300, 400, 500, 600, 700, 800])
-          }}
-          size='large'
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom height', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{
-            marginBottom: '20px',
-            height: getRandomSize([300, 400, 500, 600, 700, 800])
-          }}
-          size='large'
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with contrast prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          size='large'
-          contrast
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom style', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{
-            marginBottom: '20px',
-            fontFamily: 'Nitti, "Microsoft YaHei", 微软雅黑, monospace',
-            boxShadow: '0 1px 4px 0 hsla(0, 0%, 0%, 0.2)'
-          }}
-          round
-          size='large'
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with image prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          image={'logo'}
-          round
-          size='large'
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with video media', () => (
-    <div>
-      {urlsVideo.map(url => (
-        <MicrolinkCard
-          size='large'
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          round
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with custom data', () => (
-    <MicrolinkCard
-      url='https://microlink.io'
-      size='large'
-      data={{
-        title: 'My Custom Title',
-        image: 'https://microlink.io/logo-trim.png',
-        description: 'My Custom Description',
-        url: 'https://microlink.io'
-      }}
-    />
-  ))
-  .addWithJSX('with reverse prop', () => (
-    <div>
-      {urls.map(url => (
-        <MicrolinkCard
-          size='large'
-          key={url}
-          url={url}
-          style={{ marginBottom: '20px' }}
-          reverse
-        />
-      ))}
-    </div>
-  ))
-  .addWithJSX('with empty state', () => (
-    <MicrolinkCard size='large' url='somesitethatwontresolve.com' />
-  ))
+storiesOf('props', module)
+  .addWithJSX('default', () => urls.map(url => createMicrolink({ url })))
+  .addWithJSX('contrast', () => urls.map(url => createMicrolink({ url, contrast: true })))
+  .addWithJSX('reverse', () => urls.map(url => createMicrolink({ url, reverse: true })))
+  .addWithJSX('autoPlay', () => urlsVideo.map(url => createMicrolink({ url, autoPlay: false })))
+  .addWithJSX('loading', () => createMicrolink({url: 'somesitethatwontresolve.com'}))
+
+storiesOf('media', module)
+  .addWithJSX('image', () => urls.map(url => createMicrolink({ url, image: 'image' })))
+  .addWithJSX('logo', () => urls.map(url => createMicrolink({ url, image: 'logo' })))
+  .addWithJSX('video', () => urlsVideo.map(url => createMicrolink({ url })))
+  .addWithJSX('screenshot', () => urls.map(url => createMicrolink({
+    url,
+    image: 'screenshot',
+    screenshot: true
+  })))
+
+storiesOf('custom', module)
+  .addWithJSX('data', () => createMicrolink({
+    url: 'https://microlink.io',
+    data: {
+      title: 'My Custom Title',
+      image: 'https://microlink.io/logo-trim.png',
+      description: 'My Custom Description',
+      url: 'https://microlink.io'
+    }
+  }))
+  .addWithJSX('round', () => urls.map(url => createMicrolink({
+    url,
+    round: getRandomSize([6, 10, 20, 30, 50, 9999])
+  })))
+  .addWithJSX('width', () => urls.map(url => createMicrolink({
+    url,
+    width: getRandomSize([300, 400, 500, 600, 700, 800])
+  })))
+  .addWithJSX('height', () => urls.map(url => createMicrolink({
+    url,
+    height: getRandomSize([75, 125, 150, 175, 200, 250])
+  })))
+  .addWithJSX('style', () => urls.map(url => createMicrolink({
+    url,
+    style: {
+      marginBottom: '20px',
+      fontFamily: 'Nitti, "Microsoft YaHei", 微软雅黑, monospace',
+      boxShadow: '0 1px 4px 0 hsla(0, 0%, 0%, 0.2)'
+    }
+  })))
