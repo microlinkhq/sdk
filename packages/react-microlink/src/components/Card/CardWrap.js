6024e244ed343a93a97f69c9097d171907ad48cb
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-03-26 16:36:37 +0200

    Remove `round` prop (#96)
    * Remove `round` prop
    
    Simplify examples
    
    * Add missing font weight

--- packages/react-microlink/src/components/Card/CardWrap.js
+++ packages/react-microlink/src/components/Card/CardWrap.js
@@ -5,7 +5,7 @@ import { media, isLarge } from '../../utils'
 
 const HEIGHT = '382px'
 
-const contrastStyle = ({backgroundColor, color}) => css`
+const contrastStyle = ({ backgroundColor, color }) => css`
   background-color: ${backgroundColor};
   border-color: ${color};
   transition-property: filter;
@@ -26,23 +26,19 @@ const largeStyle = css`
 
   ${media.mobile`
     height: calc(${HEIGHT} * 7/9);
-  `}
+  `};
 `
 
 const hoverStyle = css`
   transition-property: background, border-color;
   &:hover {
-    background: #F5F8FA;
-    border-color: rgba(136,153,166,.5);
+    background: #f5f8fa;
+    border-color: rgba(136, 153, 166, 0.5);
   }
 `
 
-const reverseStyle = ({cardSize}) => css`
-  flex-direction: ${isLarge(cardSize) ? 'column-reverse' : 'row-reverse'}
-`
-
-const roundStyle = ({round}) => css`
-  border-radius: ${round === true ? `.42857em` : round};
+const reverseStyle = ({ cardSize }) => css`
+  flex-direction: ${isLarge(cardSize) ? 'column-reverse' : 'row-reverse'};
 `
 
 const style = css`
@@ -50,16 +46,16 @@ const style = css`
   background-color: #fff;
   border-width: 1px;
   border-style: solid;
-  border-color: #E1E8ED;
+  border-color: #e1e8ed;
   overflow: hidden;
   color: #181919;
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   display: flex;
   text-decoration: none;
-  opacity:1;
+  opacity: 1;
   position: relative;
 
-  transition-duration: .15s;
+  transition-duration: 0.15s;
   transition-timing-function: ease-in-out;
 
   &:active,
@@ -67,21 +63,21 @@ const style = css`
     outline: 0;
   }
 
-  ${({loading, contrast}) => !loading && !contrast && hoverStyle}
-
-  ${({round}) => round && roundStyle}
-
-  ${({cardSize}) => isLarge(cardSize) && largeStyle}
-
-  ${({reverse}) => reverse && reverseStyle}
-
-  ${({backgroundColor, color, contrast}) => contrast && color && backgroundColor && contrastStyle}
-
-  ${({backgroundColor, color, contrast}) => contrast && (!color || !backgroundColor) && hoverStyle}
+  ${({ loading, contrast }) => !loading && !contrast && hoverStyle} ${({
+  cardSize
+}) => isLarge(cardSize) && largeStyle} ${({ reverse }) =>
+  reverse && reverseStyle} ${({ backgroundColor, color, contrast }) =>
+  contrast && color && backgroundColor && contrastStyle} ${({
+  backgroundColor,
+  color,
+  contrast
+}) => contrast && (!color || !backgroundColor) && hoverStyle};
 `
 
 const CardWrap = ({ is, rel, href, target, ...props }) => {
-  const el = styled[is]`${style}`
+  const el = styled[is]`
+    ${style};
+  `
   const opts = is === 'a' ? { ...props, href, rel, target } : props
   return createElement(el, opts)
 }
