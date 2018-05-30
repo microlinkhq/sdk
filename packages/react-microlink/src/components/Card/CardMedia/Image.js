a330da3e8c32a8b0b63e22e837823bd102db0d85
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-30 17:13:10 +0200

    Ensure Card image is over HTTPS (#117)
    Close #116
    
    Waiting response from Hootsuite team:
    https://twitter.com/Kikobeats/status/1001378627559313408

--- packages/react-microlink/src/components/Card/CardMedia/Image.js
+++ packages/react-microlink/src/components/Card/CardMedia/Image.js
@@ -1,9 +1,11 @@
 import MediaWrap from './wrap'
 
+const proxyImage = url => `https://d1r1anxoiubeog.cloudfront.net/${encodeURIComponent(url)}`
+
 const defaultProps = {
   className: 'microlink_card__media microlink_card__media_image'
 }
 
 export default MediaWrap.extend.attrs(defaultProps)`
-  background-image: ${({image}) => (image ? `url('${image}')` : '')};
+  background-image: ${({image}) => (image ? `url('${proxyImage(image)}')` : '')};
 `
