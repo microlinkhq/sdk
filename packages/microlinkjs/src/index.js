e7146eb19fde04bd40c5712f02ca18322a70f1f1
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-10-05 09:43:51 +0200

    Attach package version

--- packages/microlinkjs/src/index.js
+++ packages/microlinkjs/src/index.js
@@ -5,7 +5,9 @@ const ReactDOM = require('react-dom')
 const each = require('dom101/each')
 const React = require('react')
 
-const {default: MicrolinkCard} = require('react-microlink')
+const pkg = require('../package.json')
+
+const { default: MicrolinkCard } = require('react-microlink')
 
 const BOOLEAN_STRINGS = ['true', 'false']
 
@@ -16,21 +18,25 @@ const DEFAULT_OPTS = {
 const getBoolean = str => str === 'true'
 const isStringBoolean = str => BOOLEAN_STRINGS.includes(str)
 
-const getDataAttributes = el => Object.keys(el.dataset).reduce((acc, key) => {
-  const value = el.dataset[key]
-  acc[key] = isStringBoolean(value) ? getBoolean(value) : value
-  return acc
-}, {})
+const getDataAttributes = el =>
+  Object.keys(el.dataset).reduce((acc, key) => {
+    const value = el.dataset[key]
+    acc[key] = isStringBoolean(value) ? getBoolean(value) : value
+    return acc
+  }, {})
 
-const getDOMSelector = selector => typeof selector === 'string' ? qa(selector) : [].concat(selector)
+const getDOMSelector = selector =>
+  typeof selector === 'string' ? qa(selector) : [].concat(selector)
 
 module.exports = (selector, opts) => {
   opts = Object.assign({}, DEFAULT_OPTS, opts)
 
   return each(getDOMSelector(selector), el => {
     const url = el.getAttribute('href')
-    const params = Object.assign({url}, opts, getDataAttributes(el))
+    const params = Object.assign({ url }, opts, getDataAttributes(el))
     const card = React.createElement(MicrolinkCard, params)
     ReactDOM.render(card, el)
   })
 }
+
+module.exports.version = pkg.version
