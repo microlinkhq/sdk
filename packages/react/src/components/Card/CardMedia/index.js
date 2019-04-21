43e4491575cb5b3b0ffc10c81dcf2727cfd2e2f6
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-21 12:59:07 +0200

    build: use a key per each media type

--- packages/react/src/components/Card/CardMedia/index.js
+++ packages/react/src/components/Card/CardMedia/index.js
@@ -7,15 +7,22 @@ import Video from './Video'
 
 const isUrl = url => getUrlPath(url) !== null
 
+const MEDIA_COMPONENT = {
+  video: Video,
+  image: Image
+}
+
 function CardMedia (props) {
   const { videoUrl, imageUrl, isVideo } = props
   const mediaUrl = isVideo ? videoUrl : imageUrl
   const [loading, setLoading] = useState(!isUrl(mediaUrl))
-  const MediaComponent = isVideo ? Video : Image
+  const mediaType = isVideo ? 'video' : 'image'
+  const MediaComponent = MEDIA_COMPONENT[mediaType]
+  const key = `${mediaType}__${mediaUrl}`
 
   return (
     <Fragment>
-      <MediaComponent {...props} loading={loading} />
+      <MediaComponent {...props} loading={loading} key={key} />
       {loading && (
         <ImageLoadCatcher src={mediaUrl} onLoad={() => setLoading(false)} />
       )}
