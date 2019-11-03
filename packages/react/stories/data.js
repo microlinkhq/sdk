5463d10d62dddafcad8c3aea30e9a3a42bba3c7e
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-03 22:10:10 +0100

    build: setup links

--- packages/react/stories/data.js
+++ packages/react/stories/data.js
@@ -1,34 +1,19 @@
 'use strict'
 
-export const urls = [
-  'https://twitter.com/stripe/status/750230305399681024',
-  'https://medium.com/@the_economist/apple-should-shrink-its-finance-arm-before-it-goes-bananas-f7fcdc754091',
-  'https://www.theverge.com/2017/10/27/16145498/insecure-broad-city-high-maintenance-web-series-hbo-comedy-central',
-  'https://techcrunch.com/2017/10/26/super-mario-odyssey-review-a-masterpiece-of-twists-and-turns/',
-  'http://mashable.com/2017/10/19/cybersecurity-hacker-online-course',
-  'https://www.engadget.com/es/2017/10/23/meizu-m6-note-analisis-review-fotos/',
-  'https://www.nytimes.com/2017/09/19/learning/whats-going-on-in-this-graph-sept-19-2017.html',
-  'https://gizmodo.com/drone-video-of-border-wall-prototypes-accidentally-show-1819710328',
-  'https://www.bbc.com/news/technology-40762328',
-  'https://browserless.js.org',
-  'https://xn--vi8hl0c.ws/',
-  'https://media.giphy.com/media/LqTSLCsIIkCTvQ8X9g/giphy.gif'
-]
+import demoLinks from '@microlink/demo-links'
+import { chain } from 'lodash'
 
-export const urlsVideo = [
-  'https://www.facebook.com/afcajax/videos/1686831701364171',
-  'https://gfycat.com/gifs/detail/timelyhealthyarmadillo',
-  'https://plays.tv/video/5a6f64b1bef69a7fa9/holy-shit',
-  'https://streamable.com/46ont',
-  'https://www.instagram.com/p/BXHj-DllyYU/',
-  'https://www.amazon.com/All-new-Echo-Plus-Bundle-Philips/dp/B07H1QBW2L',
-  'https://twitter.com/verge/status/957383241714970624',
-  'https://vimeo.com/188175573',
-  'https://9gag.com/gag/aMZ1WbW',
-  'https://www.youtube.com/watch?v=hwMkbaS_M_c'
-]
+export const urls = chain(demoLinks)
+  .filter(link => !link.audio && !link.video)
+  .map('url')
+  .value()
 
-export const urlsAudio = [
-  'https://www.ivoox.com/aluvion-presentaciones-viene-audios-mp3_rf_41524123_1.html',
-  'https://syntax.fm/show/180/potluck-why-webpack-serverless-agencies-recruiters-css-grid-mq-in-styled-components'
-]
+export const urlsVideo = chain(demoLinks)
+  .filter('video')
+  .map('url')
+  .value()
+
+export const urlsAudio = chain(demoLinks)
+  .filter('audio')
+  .map('url')
+  .value()
