68a6e97ce953f6989a9639aa75d67ef92b157233
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-04 09:57:15 +0100

    refactor: less is more

--- packages/react/stories/data.js
+++ packages/react/stories/data.js
@@ -1,19 +1,14 @@
 'use strict'
 
 import demoLinks from '@microlink/demo-links'
-import chain from 'lodash/chain'
+import filter from 'lodash/filter'
 
-export const urls = chain(demoLinks)
-  .filter(link => !link.audio && !link.video)
-  .map('url')
-  .value()
-
-export const urlsVideo = chain(demoLinks)
-  .filter('video')
-  .map('url')
-  .value()
-
-export const urlsAudio = chain(demoLinks)
-  .filter('audio')
-  .map('url')
-  .value()
+export const urls = filter(demoLinks, link => !link.audio && !link.video).map(
+  link => link.url
+)
+export const urlsVideo = filter(demoLinks, link => link.video).map(
+  link => link.url
+)
+export const urlsAudio = filter(demoLinks, link => link.audio).map(
+  link => link.url
+)
