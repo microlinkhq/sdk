131d621d3f1c439598dcb80259217782001d5297
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-09 18:35:11 +0100

    test: update snapshots

--- packages/react/stories/data.js
+++ packages/react/stories/data.js
@@ -2,13 +2,17 @@
 
 import demoLinks from '@microlink/demo-links'
 import filter from 'lodash/filter'
+import take from 'lodash/take'
 
-export const urls = filter(demoLinks, link => !link.audio && !link.video).map(
-  link => link.url
+export const urls = take(
+  filter(demoLinks, link => !link.audio && !link.video).map(link => link.url),
+  3
 )
-export const urlsVideo = filter(demoLinks, link => link.video).map(
-  link => link.url
+export const urlsVideo = take(
+  filter(demoLinks, link => link.video).map(link => link.url),
+  3
 )
-export const urlsAudio = filter(demoLinks, link => link.audio).map(
-  link => link.url
+export const urlsAudio = take(
+  filter(demoLinks, link => link.audio).map(link => link.url),
+  3
 )
