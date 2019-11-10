b2869c1782d568762ecb23594dc8bcfbfa825cf0
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-10 10:29:47 +0100

    ci: debug

--- packages/react/stories/data.js
+++ packages/react/stories/data.js
@@ -1,23 +1,22 @@
 'use strict'
 
-const demoLinks = require('@microlink/demo-links')
-const { chain } = require('lodash')
+import demoLinks from '@microlink/demo-links'
+import { pick, chain } from 'lodash'
 
-const getData = (fn) => chain(demoLinks)
+const { NODE_ENV } = process.env
+
+const isTesting = NODE_ENV === 'test'
+
+const getData = (fn, pickProp) => chain(
+  isTesting ? pick(demoLinks, pickProp) : demoLinks
+)
   .toPairs()
   .filter(fn)
-  .sortBy(([name, link]) => name)
-  .take(3)
-  .fromPairs()
-  .map('url')
+  .take(NODE_ENV === 'test' ? 1 : Infinity)
+  .map('1.url')
   .value()
+  .sort()
 
-exports.urls = getData(([name, link]) => !link.audio && !link.video)
-exports.urlsVideo = getData(([name, link]) => link.video)
-exports.urlsAudio = getData(([name, link]) => link.audio)
-
-console.log({
-  urls: exports.urls,
-  urlsVideo: exports.urlsVideo,
-  urlsAudio: exports.urlsAudio
-})
+export const urls = getData(([name, link]) => !link.audio && !link.video, 'Apple')
+export const urlsVideo = getData(([name, link]) => link.video, 'Facebook')
+export const urlsAudio = getData(([name, link]) => link.audio, 'Spotify')
