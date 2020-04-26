ae4be8516d1e8e821ef3a0521813b0794c9fa646
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-26 11:03:42 +0200

    test: fix jest mock reference

--- packages/react/stories/stories.test.js
+++ packages/react/stories/stories.test.js
@@ -4,7 +4,7 @@ import initStoryshots from '@storybook/addon-storyshots'
 import 'jest-styled-components'
 
 jest.mock('@microlink/mql', () =>
-  require('@microlink/mql/' + require('@microlink/mql/package.json').module)
+  require('@microlink/mql/' + require('@microlink/mql/package.json').browser)
 )
 
 initStoryshots()
