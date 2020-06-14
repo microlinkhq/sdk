db9efceb3c6268cfc7b7d664cb0dee28d47cf669
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-14 13:36:41 +0200

    build: add hover-vanilla

---
+++ packages/hover-vanilla/rollup.config.js
@@ -0,0 +1,74 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import visualizer from 'rollup-plugin-visualizer'
+import filesize from 'rollup-plugin-filesize'
+import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+
+const plugins = ({ compress }) => [
+  commonjs(),
+  nodeResolve(),
+  compress && terser(),
+  filesize(),
+  visualizer({ template: 'treemap' }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  })
+]
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'styled-components': 'styled',
+  '@microlink/mql': 'mql'
+}
+
+const build = ({ file, format, name, exports }) => {
+  const compress = file.includes('.min.')
+  return {
+    input: './src/index.js',
+    output: {
+      sourcemap: compress,
+      file,
+      format,
+      exports,
+      name,
+      globals
+    },
+    external: Object.keys(globals),
+    plugins: plugins({ compress })
+  }
+}
+
+export default [
+  build({
+    format: 'umd',
+    file: 'dist/microlink.js',
+    name: 'microlinkHover'
+  })
+  // build({
+  //   format: 'umd',
+  //   file: 'dist/microlink.min.js',
+  //   name: 'microlinkHover'
+  // }),
+  // build({
+  //   format: 'esm',
+  //   file: 'dist/microlink.module.js',
+  //   exports: 'named'
+  // }),
+  // build({
+  //   format: 'esm',
+  //   file: 'dist/microlink.min.module.js',
+  //   exports: 'named'
+  // }),
+  // build({
+  //   format: 'cjs',
+  //   file: 'dist/microlink.cjs.js',
+  //   exports: 'named'
+  // }),
+  // build({
+  //   format: 'cjs',
+  //   file: 'dist/microlink.cjs.min.js',
+  //   exports: 'named'
+  // })
+]
---
+++ packages/hover-vanilla/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { withHover } from '@microlink/hover-react'
+
+function parseJSON (value) {
+  try {
+    return JSON.parse(value)
+  } catch (err) {
+    return value
+  }
+}
+
+function parseObject (obj) {
+  return Object.keys(obj).reduce(function (acc, key) {
+    return Object.assign(acc, { [key]: parseJSON(obj[key]) })
+  }, {})
+}
+
+function getDOMSelector (selector) {
+  return typeof selector === 'string'
+    ? document.querySelectorAll(selector)
+    : [].concat(selector).filter(Boolean)
+}
+
+function forEach (list, fn) {
+  for (let i = 0; i < list.length; i++) fn(list[i])
+}
+
+function microlink (selector, opts, rootNode) {
+  return forEach(getDOMSelector(selector), function (el) {
+    ReactDOM.render(
+      React.createElement(
+        withHover,
+        Object.assign(
+          {
+            as: 'div',
+            url: el.getAttribute('href'),
+            className: el.className,
+            children: el.text
+          },
+          opts,
+          parseObject(el.dataset)
+        )
+      ),
+      rootNode || el
+    )
+  })
+}
+
+microlink.version = '__VERSION__'
+
+export default microlink
