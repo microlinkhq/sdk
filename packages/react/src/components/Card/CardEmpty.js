67f75ed5123856f0dd17e91b5c1d4b897994e1e1
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-14 15:14:12 +0200

    fix: linter

--- packages/react/src/components/Card/CardEmpty.js
+++ packages/react/src/components/Card/CardEmpty.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 import { emptyStateAnimation, emptyStateImageAnimation } from './CardAnimation'
 import CardImage from './CardMedia/Image'
