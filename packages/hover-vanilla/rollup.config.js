a7b52bacc8e94322fedf7a79b6fd90f961f50374
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-11-13 23:45:50 +0100

    build: update dependencies (#292)

--- packages/hover-vanilla/rollup.config.js
+++ packages/hover-vanilla/rollup.config.js
@@ -1,9 +1,9 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
-import visualizer from 'rollup-plugin-visualizer'
+import { visualizer } from 'rollup-plugin-visualizer'
 import commonjs from '@rollup/plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
-import { terser } from 'rollup-plugin-terser'
 import replace from '@rollup/plugin-replace'
+import terser from '@rollup/plugin-terser'
 import copy from 'rollup-plugin-copy'
 
 const isProduction = process.env.NODE_ENV === 'production'
