2e7a8b1737956556b02eee49f569a81f08c5fef5
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-03-29 19:34:45 +0200

    refactor: use localhost-url-regex (#299)
    instead of is-localhost-url since it's deprecated

--- packages/react/src/utils/index.js
+++ packages/react/src/utils/index.js
@@ -1,5 +1,5 @@
 import { fetchFromApi, getApiUrl as createApiUrl } from '@microlink/mql'
-import isLocalhostUrl from 'is-localhost-url'
+import localhostUrl from 'localhost-url-regex'
 import { css } from 'styled-components'
 
 export const isSSR = typeof window === 'undefined'
@@ -81,7 +81,7 @@ export const isLarge = cardSize => cardSize === 'large'
 export const isSmall = cardSize => cardSize === 'small'
 
 export const imageProxy = url =>
-  isLocalhostUrl(url)
+  localhostUrl().test(url)
     ? url
     : `https://images.weserv.nl/?${new URLSearchParams({
         url,
